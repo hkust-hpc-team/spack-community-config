64bea461fb8ffc177de69677b6738ed73253c484
kftse-ust-hk <kftse@ust.hk>
2025-09-05 16:20:47 +0800

    config for cpan

--- templates/modules/perl-modulefile.lua
+++ templates/modules/perl-modulefile.lua
@@ -9,4 +9,10 @@ setenv("PERL_LOCAL_LIB_ROOT", perl5_home)
 setenv("PERL_MB_OPT", "--install_base \"" .. perl5_home .. "\"")
 setenv("PERL_MM_OPT", "INSTALL_BASE=" .. perl5_home)
 prepend_path("PATH", perl5_home .. "/bin")
+
+-- CPAN-specific settings
+local cpan_home = os.getenv("HOME") .. "/.cpan"
+execute{cmd = "mkdir -p " .. cpan_home, modeA={"load"}}
+setenv("CPAN_HOME", cpan_home)
+setenv("PERL_CPAN_MIRROR", "http://www.cpan.org/")
 {% endblock %}
