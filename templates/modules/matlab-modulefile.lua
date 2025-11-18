62ecf2fe07dd0260c3f50e432d95087fab63ede8
kftse <kftse@kftse.io>
2025-11-18 12:39:00 +0800

    fix module env not working

--- templates/modules/matlab-modulefile.lua
+++ templates/modules/matlab-modulefile.lua
@@ -1,6 +1,6 @@
 {% extends "modules/modulefile.lua" %}
 {% block footer %}
-local matlab_user_home = "~/.matlab/{{ spec.version }}-{{ spec.dag_hash()[:7] }}"
+local matlab_user_home = os.getenv("HOME") .. "/.matlab/{{ spec.version }}"
 execute{cmd = "mkdir -p " .. matlab_user_home, modeA={"load"}}
-setenv("MATLABPATH", matlab_user_home)
+prepend_path("MATLABPATH", matlab_user_home)
 {% endblock %}
